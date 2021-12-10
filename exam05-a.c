3d15fa22a938c286adc1f8e90f8d379556396e4b
MamatNik01 <91784286+MamatNik01@users.noreply.github.com>
2021-12-10 19:33:31 +0300

    Update exam05-a.c

--- exam05-a.c
+++ exam05-a.c
@@ -1,41 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <unistd.h>
-
-double f(double x)
+int a1, b1;
+int a2, b2;
+void input1()
 {
-    return -3.0f * cosf(x);
+    printf("[1] a|b: ");
+    scanf("%d %d", &a1, &b1);
 }
-
+void input2()
+{
+    printf("[2] a|b: ");
+    scanf("%d %d", &a2, &b2);
+}
+void print()
+{
+    printf("[1]: %d / %d\n", a1, b1);
+    printf("[2]: %d / %d\n", a2, b2);
+}
+void add()
+{
+   int a3 = a1 * b2 + a2 * b1;
+   int b3 = b1 * b2;
+   printf("%d / %d\n", a3, b3);
+}
+void subtract()
+{
+    int a3 = a1 * b2 - a2 * b1;
+    int b3 = b1 * b2;
+    printf("%d / %d\n", a3, b3);
+}
 int main()
 {
-    double a, b;
-    printf ("a, b: ");
-    scanf("%lf %lf", &a, &b);
-    
-    double e;
-    printf ("e: ");
-    scanf("%lf", &e);
-
-    while (1)
+    int choice;
+    do
     {
-        double c = (a + b) / 2.0;
-        
-        float fa = f(a);
-        float fc = f(c);
-        
-        if (fabsf(fc) <= e)
+        printf("0 - exit\n");
+        printf("1 - input #1\n");
+        printf("2 - input #2\n");
+        printf("3 - print\n");
+        printf("4 - add\n");
+        printf("5 - subtract\n");
+        printf("choice: ");
+        scanf("%d", &choice);
+        switch (choice)
         {
-            printf("x: %.5f\n", c);
-            printf("y: %.5f\n", fc);
-            break;
+            case 1: input1(); break;
+            case 2: input2(); break;
+            case 3: print(); break;
+            case 4: add(); break;
+            case 5: subtract(); break;
         }
-        
-        if (fa * fc > 0.0f)
-        a = c;
-        else
-        b = c;
+        printf("\n"); 
     }
+    while (choice != 0);
     return 0;
 }
